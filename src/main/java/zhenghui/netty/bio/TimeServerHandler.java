6fd218a4d862036561da1aa7a02a1943f5e54e3d
zhenghui <jingbo2759@gmail.com>
2014-10-27 23:02:23 +0800

    'init'

---
+++ src/main/java/zhenghui/netty/bio/TimeServerHandler.java
@@ -0,0 +1,43 @@
+package zhenghui.netty.bio;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+/**
+ * User: zhenghui
+ * Date:2014/10/27
+ * Time:21:45
+ * Email:jingbo2759@163.com
+ *
+ * BIO 服务端处理器
+ */
+public class TimeServerHandler implements Runnable{
+
+    private Socket socket;
+
+    public TimeServerHandler(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        try {
+            InputStream inputStream = socket.getInputStream();
+            OutputStream outputStream = socket.getOutputStream();
+            byte[] buffer = new byte[128];
+            while (inputStream.read(buffer) !=-1){
+                String msg = new String(buffer);
+                System.out.println(new String(buffer));
+                if (msg.startsWith("hello")){
+                    outputStream.write("hello client.".getBytes());
+                }
+            }
+            outputStream.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
